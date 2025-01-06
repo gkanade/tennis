8fdee96fd518370c90fff5b3bc48ea119ae0be9a
gkanade <gaurav.kanade@gmail.com>
2025-01-05 16:42:54 -0800

    Update api.py

--- api.py
+++ api.py
@@ -3,7 +3,7 @@ import requests
 API_KEY = "a7342feeb4msh25abe0aad58e0f8p1995e1jsne02c951571ab"
 API_HOST = "tennis-live-data.p.rapidapi.com"
 
-url = "https://tennis-live-data.p.rapidapi.com/matches-by-date/2020-09-06"
+url = "https://tennis-live-data.p.rapidapi.com/tournaments/ATP/2020"
 
 headers = {
     "x-rapidapi-key": API_KEY,
